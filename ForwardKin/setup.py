8df0a24265e5bd8acc049744b858047f148a2743
Marcin <marcin.polosak@wp.pl>
2024-04-03 22:08:59 +0200

    final homework

---
+++ ForwardKin/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'ForwardKin'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name), glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marcin',
+    maintainer_email='marcin.polosak@wp.pl',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ForwardKin = ForwardKin.ForwardKin:main'
+        ],
+    },
+)
--- urdf_tutorial/launch/display.launch.py
+++ urdf_tutorial/launch/display.launch.py
@@ -2,20 +2,21 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
     urdf_tutorial_path = FindPackageShare('urdf_tutorial')
-    default_model_path = PathJoinSubstitution(['urdf', '01-myfirst.urdf'])
-    default_rviz_config_path = PathJoinSubstitution([urdf_tutorial_path, 'rviz', 'urdf.rviz'])
+    default_model_path = PathJoinSubstitution(['dobot_visual.urdf.xacro'])
+    default_rviz_config_path = PathJoinSubstitution(['dobot_config.rviz'])
 
     # These parameters are maintained for backwards compatibility
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
                                     description='Flag to enable joint_state_publisher_gui')
     ld.add_action(gui_arg)
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+    rviz_arg = DeclareLaunchArgument(name='rviz', default_value=default_rviz_config_path,
                                      description='Absolute path to rviz config file')
     ld.add_action(rviz_arg)
 
@@ -27,9 +28,15 @@ def generate_launch_description():
         PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
         launch_arguments={
             'urdf_package': 'urdf_tutorial',
-            'urdf_package_path': LaunchConfiguration('model'),
-            'rviz_config': LaunchConfiguration('rvizconfig'),
+            'urdf_package_path': PathJoinSubstitution(['urdf', LaunchConfiguration('model')]),
+            'rviz_config': PathJoinSubstitution([urdf_tutorial_path, 'rviz', LaunchConfiguration('rviz')]),
             'jsp_gui': LaunchConfiguration('gui')}.items()
     ))
+
+    ld.add_action(Node(
+            package='ForwardKin',
+            executable='ForwardKin',
+            name='my_node',
+        ),)
 
     return ld
